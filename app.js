cc2313172dc287ac6454548093431cb5b0c90aaa
Errytagedesign <errytage@gmail.com>
2023-07-04 00:10:28 +0100

    Render book list, and created remove book functions, plus data persist in local storage

--- app.js
+++ app.js
@@ -2,13 +2,17 @@
 const AddBookBtn = document.querySelector('.addBook');
 const bookTitle = document.querySelector('#title');
 const bookAuthor = document.querySelector('#author');
+const booksContainer = document.querySelector('.booksContainer');
 
 // Decalre bookCollections array
-const bookCollections = [];
+const bookCollections = JSON.parse(window.localStorage.getItem('books')) || [];
 
 // Decalre a addBooks function, push the value of title and author in the bookCollection array
 function addBooks() {
-  bookCollections.push({ title: bookTitle.value, author: bookAuthor.value });
+  bookCollections.push({
+    title: bookTitle.value,
+    author: bookAuthor.value,
+  });
 }
 
 // Decalre a clearInput function, to clear the input value unpon clicking the button
@@ -17,10 +21,53 @@ function clearInput() {
   bookAuthor.value = '';
 }
 
+// Save bookCollections to localstorage
+function saveBooks() {
+  window.localStorage.setItem('books', JSON.stringify(bookCollections));
+}
+
 // Add eventListener to the button, to run each both functions everytime the button is clicked
 AddBookBtn.addEventListener('click', (e) => {
   e.preventDefault();
   addBooks();
+  saveBooks();
+  renderBooks();
   clearInput();
-  console.log(bookCollections);
+});
+
+function removeBook(id) {
+  const indexOfBookToRemove = bookCollections.filter((book) => book.id === id);
+
+  if (indexOfBookToRemove !== -1) {
+    bookCollections.splice(indexOfBookToRemove, 1);
+    saveBooks();
+    renderBooks();
+  }
+}
+
+function renderBooks() {
+  const storedBooks = JSON.parse(window.localStorage.getItem('books'));
+
+  if (storedBooks) {
+    let displayBook = storedBooks.map(
+      (book, index) =>
+        `
+            <article class="d-flex flex-row justify-content-between">
+
+            <h2> ${book.title} </h2>
+            <p> ${book.author} </p>
+
+            <button onclick="removeBook(${index})" class="removeBtn"> Remove </button>
+
+            </article>
+
+            `,
+    );
+
+    booksContainer.innerHTML = displayBook.join('');
+  }
+}
+
+window.addEventListener('DOMContentLoaded', () => {
+  renderBooks();
 });
